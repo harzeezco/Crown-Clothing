1bfb18ffd2800933ebed7c84684e518d435c963a
Wakil Azeez <wakilabdulazeez@gmail.com>
2023-05-31 09:33:54 +0100

    add some changes to the shop route

--- src/routes/Shop/shop-component.jsx
+++ src/routes/Shop/shop-component.jsx
@@ -9,7 +9,7 @@ const Shop = () => {
     <>
       <Routes>
         <Route index element={<CategoriesPreview />} />
-        <Route path="/:category" element={<Category />} />
+        <Route path=":category" element={<Category />} />
       </Routes>
     </>
   );
